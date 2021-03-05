bc7c76197e0b557e0b7c370be7cb8698304d2c0d
alu0101203003 <alu0101203003@ull.edu.es>
2021-03-05 22:44:18 +0000

    ejercicio 4 hecho

---
+++ src/ejercicio-5.ts
@@ -0,0 +1,7 @@
+const onePunch = (s :string) => s==''? ('Broken!'):(s.split(" ").sort().join(" ").replace(/a|e/g,""));
+
+console.log(onePunch('Beard Jeans Hairbrush Knuckleduster Sand'))           //=>‘Brd Hirbrush Jns Knuckldustr Snd’
+console.log(onePunch('Sock Beard Vest Lady Sage'))                          //=>’Brd Ldy Sg Sock Vst’
+console.log(onePunch('Beard Sack Gun Parachute Face-Kicking-Shoes'))        //=>’Brd Fc-Kicking-Shos Gun Prchut Sck’
+console.log(onePunch('Snot Snow Soda Tank Beard'))                          //=>’Brd Snot Snow Sod Tnk’
+console.log(onePunch(''))                                                   //=>’Broken!’
