f610adfd321fce39e13ae2dedf52046114e08da9
alu0101203003 <alu0101203003@ull.edu.es>
2021-03-06 00:02:02 +0000

    ejercicio 6 hecho

--- src/ejercicio-3.ts
+++ src/ejercicio-3.ts
@@ -1,25 +1,26 @@
 function isValid (cadena :string) {
-            for(var i :number = 0; i < cadena.length; i++){
-            var cont = 0;
-            var bloque :string = cadena.slice(i,cadena.length);
-            var bloque_sz :number = parseInt(bloque);
-            if (isNaN(bloque_sz)){
-                return false;
-            }
-            var digito :string = bloque_sz.toString();
-            bloque_sz = bloque_sz + digito.length;
-            for(var j :number = i+digito.length; j < i + bloque_sz; j++){
-                var pos :number = parseInt(cadena[j]);
-                if (isNaN(pos) && j < cadena.length){ // se cuentan las letras
-                    cont++;
-                }
-            }
-            i = i + bloque_sz - 1;
-            if ((bloque_sz - digito.length) != cont){ // si el numero de letras no coincide con el tamaño del bloque: no es valido
-                return false;
+    for(var i :number = 0; i < cadena.length; i++){
+        var cont = 0;
+        var bloque :string = cadena.slice(i,cadena.length);
+        var bloque_sz :number = parseInt(bloque);
+        if (isNaN(bloque_sz)){
+            return false;
+        }
+        var digito :string = bloque_sz.toString();
+        bloque_sz = bloque_sz + digito.length;
+        for(var j :number = i+digito.length; j < i + bloque_sz; j++){
+            var pos :number = parseInt(cadena[j]);
+            if (isNaN(pos) && j < cadena.length){ // se cuentan las letras
+                cont++;
             }
         }
-        return true;
+        i = i + bloque_sz - 1;
+        if ((bloque_sz - digito.length) != cont){ // si el numero de letras no coincide con el tamaño del bloque: no es valido
+            return false;
+        }
     }
-    console.log(isValid("3hey5hello2hi"));
-    console.log(isValid("4code10helloworld"));
+    return true;
+}
+
+console.log(isValid("3hey5hello2hi"));
+console.log(isValid("4code10helloworld"));
---
+++ src/ejercicio-6.ts
@@ -0,0 +1,33 @@
+function isValidISBN(s :string){
+s = s.split("-").join("");
+if (s.length != 10){
+    return false;
+}
+var valor = []
+for (var i :number = 0; i < 10; i++){
+    if (i!=9 && isNaN(parseInt(s[i])) || (i==9 && s[i]!='X' && isNaN(parseInt(s[i])))){
+        return false;
+    }
+    if (s[i]=='X'){
+        valor[i] = 10;
+    } else {
+        valor[i] = parseInt(s[i]);
+    }
+}
+valor = valor.reverse(); // se invierte el array para facilitar el sumatorio
+var sumatorio :number = 0;
+for (var i :number = 10; i >= 1; i--){
+    sumatorio = sumatorio + (valor[i-1]*i);
+}
+if (sumatorio%11 == 0){
+    return true;
+} else {
+    return false;
+}
+
+}
+
+console.log(isValidISBN('3-598-21507-X'))
+console.log(isValidISBN('3-598-21508-8'))
+console.log(isValidISBN('0-100-00000-0'))
+console.log(isValidISBN('0-000-00000-0'))
